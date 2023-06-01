24ff1f339cc928b37c0b0d1884aba6974d78f1e9
Renanbfreitas <renanbf1992@outlook.com>
2023-06-01 16:35:20 -0300

    feat: add req 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    for num in nums:
+        if type(num) != int or num < 0:
+            return False
+        if nums.count(num) > 1:
+            return num
+    return False
+
+
+# Teste do Requisito
+# print(find_duplicate([1, 3, 4, 2, 2]))
